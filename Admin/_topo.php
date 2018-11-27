15403e8283bb9bcecab9fbb6579dc49c27c088ca
Thiago Almeida <thiagoalmeida.negocios@gmail.com>
2018-11-27 00:26:22 -0200

    incluída mais uma opção de alteração do cadastro Admin

--- Admin/_topo.php
+++ Admin/_topo.php
@@ -4,7 +4,7 @@
             <span class="sr-only">Toggle navigation</span>
             <span class="icon-bar"></span>
             <span class="icon-bar"></span>
-            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>            
         </button>
         <a class="navbar-brand" href="index.php">Control Service</a> 
     </div> 
@@ -12,7 +12,7 @@
     <div style="color: white;
          padding: 15px 50px 5px 50px;
          float: right;
-         font-size: 16px;"> Seja bem-vindo, <?= $_SESSION['email'] ?> &nbsp; <a href="_menu.php?close=1" class="btn btn-danger square-btn-adjust">Sair</a> </div>
+         font-size: 16px;"> Seja bem-vindo<?= $_SESSION['perfil'] == 2 ? ', <a href="alterar_cliente.php?cod=' . $_SESSION['cod'] . '">' . $_SESSION['email'] . '</a>': ', ' . $_SESSION['email'] ?> &nbsp; <a href="_menu.php?close=1" class="btn btn-danger square-btn-adjust">Sair</a> </div>
     <?php } ?>
 </nav>
 
